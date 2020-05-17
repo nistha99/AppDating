35560771bf63d4e9620bf8e60b3661ce67e8fe3e
Nistha <wanderer.april@gmail.com>
2020-05-18 01:15:21 +0545

    Initial Commit

---
+++ AppDating.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+
+using AppDating.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppDating.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
